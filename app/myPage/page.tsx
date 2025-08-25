d12ed4a7ed2f93359312e756231f68f7c38bda7d
Morita <168296824+Kainn1203@users.noreply.github.com>
2025-08-25 10:04:14 +0900

    update: package.jsonなどの修正

--- app/myPage/page.tsx
+++ app/myPage/page.tsx
@@ -1,10 +1,14 @@
-const MyPage = () => {
+import { getMe } from "@/lib/supabase/me";
+import { redirect } from "next/navigation";
+
+export default async function MyPage() {
+  const me = await getMe();
+  if (!me) redirect("/login");
+
   return (
     <>
       <h1>Hello MyPage</h1>
-      <p>こんな感じで書いていく</p>
+      <p>私の名前は{me.name}です</p>
     </>
   );
-};
-
-export default MyPage;
+}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "avatars.slack-edge.com" },
+      { protocol: "https", hostname: "ca.slack-edge.com" },
+      { protocol: "https", hostname: "a.slack-edge.com" }, // 予備
+      { protocol: "https", hostname: "secure.gravatar.com" }, // ← 今回のエラーの本丸
+      { protocol: "https", hostname: "www.gravatar.com" }, // 予備
+      {
+        protocol: "https",
+        hostname: "dxzemwwaldgwnjkviyfn.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      }, // supabase
+    ],
+  },
 };
 
 export default nextConfig;
